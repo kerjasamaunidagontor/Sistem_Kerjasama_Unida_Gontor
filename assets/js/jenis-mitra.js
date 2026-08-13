c8ee81b92642acdd6e59997360cf139eb107a3eb
kerjasamaunidagontor <kerjasamaunidagontor@gmail.com>
2026-08-13 14:29:39 +0700

    Update API_BASE URL and refactor code

--- assets/js/jenis-mitra.js
+++ assets/js/jenis-mitra.js
@@ -2,7 +2,7 @@
 // CONFIG
 // ===============================
 window.API_BASE =
-  "https://script.google.com/macros/s/AKfycbzHzKcm-fPEVOfKeU9iWoC3OcaDiR-G2hoMEh868zO1d0KpGeTUXI8sA1ljP658gjSWxQ/exec";
+  "https://script.google.com/macros/s/AKfycbznrVnZKXyiCihy6xh4x0bv1ihMqKROPaCHFzAF05uXawR5LWetMMoMgx_0azPpNYvrSA/exec";
 
 let JENIS_MITRA = [];
 let editRow = null;
